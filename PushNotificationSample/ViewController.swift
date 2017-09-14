8b00ed431a0dd6fb375b4ce4ba0dc5f9b3f69f4c
Sujan Vaidya <shujan1992@gmail.com>
2017-09-14 17:00:55 +0545

    Initial setup

---
+++ PushNotificationSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PushNotificationSample
+//
+//  Created by Sujan Vaidya on 9/14/17.
+//  Copyright © 2017 Sujan Vaidya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
