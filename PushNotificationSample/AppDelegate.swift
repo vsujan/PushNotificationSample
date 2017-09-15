4a9282e2612ae9b040093c0ae405e32fd339c806
Sujan Vaidya <shujan1992@gmail.com>
2017-09-15 15:48:13 +0545

    Add delegates for pushnotification and alert based on whether state of the app

--- PushNotificationSample/AppDelegate.swift
+++ PushNotificationSample/AppDelegate.swift
@@ -7,40 +7,84 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+  
   var window: UIWindow?
-
-
+  var fromLaunch = false
+  
+  
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
+    if #available(iOS 10.0, *) {
+      let mynotif = UNUserNotificationCenter.current()
+      mynotif.requestAuthorization(options: [.alert, .sound, .badge]) {(granted, error) in
+        guard granted else { return }
+        self.getNotificationSettings()
+      }
+    } else {
+      // Fallback on earlier versions
+      UIApplication.shared.registerUserNotificationSettings(UIUserNotificationSettings(types: [.badge, .sound, .alert], categories: nil))
+      UIApplication.shared.registerForRemoteNotifications()
+      //      UIApplication.shared.register
+    }
+    
+    //sujan-note When the app is in close state
+    if let _ = launchOptions?[.remoteNotification] as? [String: AnyObject] {
+      fromLaunch = true
+    }
+    
     return true
   }
+  
+  func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+    //    let tokenString = deviceToken.reduce("", {$0 + String(format: "%02X", $1)})
+    let tokenString = deviceToken.reduce("") { string, byte in
+      string + String(format: "%02X", byte)
+    }
+    print("Device Token: \(tokenString)")
+  }
+  
+  func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+    print("Failed to register: \(error)")
+  }
+  
+  func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+    //sujan-note When the app is in foreground or background
+    
+//    let aps = userInfo["aps"] as! [String: AnyObject]
+//    print(aps)
+    
+    let text = fromLaunch ? "From did launch" : "From did receive"
+    fromLaunch = false
+    
+    let alert = UIAlertController(title: "Simple", message: text, preferredStyle: .alert)
+    
+    alert.addAction(UIAlertAction(title: "Destructive", style: .destructive) { _ in
+      print("Destructive")
+    })
+    alert.addAction(UIAlertAction(title: "OK", style: .default) { _ in
+      print("OK")
+    })
+    
+    self.window?.rootViewController?.present(alert, animated: true, completion: nil)
 
-  func applicationWillResignActive(_ application: UIApplication) {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+  }
+  
+  func getNotificationSettings() {
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().getNotificationSettings { (settings) in
+        print("Notification settings: \(settings)")
+        guard settings.authorizationStatus == .authorized else { return }
+        UIApplication.shared.registerForRemoteNotifications()
+      }
+    } else {
+      // Fallback on earlier versions
+    }
   }
 
-  func applicationDidEnterBackground(_ application: UIApplication) {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-  }
-
-  func applicationWillEnterForeground(_ application: UIApplication) {
-    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-  }
-
-  func applicationDidBecomeActive(_ application: UIApplication) {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-  }
-
-  func applicationWillTerminate(_ application: UIApplication) {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-  }
-
-
+  
 }
 
--- PushNotificationSample/ViewController.swift
+++ PushNotificationSample/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+  var myLabel: UILabel!
+  var text = ""
 
   override func viewDidLoad() {
     super.viewDidLoad()
+    setup()
     // Do any additional setup after loading the view, typically from a nib.
   }
 
+  func setup() {
+    myLabel = UILabel()
+    myLabel.text = text
+    self.view.addSubview(myLabel)
+  }
+  
+  override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
+    myLabel.frame = CGRect(x: 12, y: 40, width: 300, height: 20);
+    
+  }
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
